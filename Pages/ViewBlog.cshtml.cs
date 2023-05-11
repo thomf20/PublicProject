dfa317668e53ff3eb2ceaa0e8d8b7f3afea383c9
thomf20 <thom.flodqvist@gmail.com>
2023-05-11 09:36:58 +0100

    Fixat ny sida för blogginlägg

--- Pages/ViewBlog.cshtml.cs
+++ Pages/ViewBlog.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PublicProject.Models;
 
 namespace PublicProject.Pages
 {
     public class ViewBlogModel : PageModel
     {
-        public void OnGet()
+        private readonly Data.ApplicationDbContext dbContext; // Byt ut "YourDbContext" med namnet på din databaskontext
+
+        public Blog BlogPost { get; set; }
+
+        public ViewBlogModel(Data.ApplicationDbContext dbContext) // Byt ut "YourDbContext" med namnet på din databaskontext
         {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            BlogPost = dbContext.Blog.Find(id);
+
+            if (BlogPost == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
