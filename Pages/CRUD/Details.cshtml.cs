a77373721f52a01a281d4968292faf058fb4f472
thomf20 <thom.flodqvist@gmail.com>
2023-05-11 08:43:58 +0100

    Lagt till CRUD

---
+++ Pages/CRUD/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PublicProject.Data;
+using PublicProject.Models;
+
+namespace PublicProject.Pages.CRUD
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly PublicProject.Data.ApplicationDbContext _context;
+
+        public DetailsModel(PublicProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      public Blog Blog { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Blog == null)
+            {
+                return NotFound();
+            }
+
+            var blog = await _context.Blog.FirstOrDefaultAsync(m => m.Id == id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Blog = blog;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/CRUD/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PublicProject.Data;
+using PublicProject.Models;
+
+namespace PublicProject.Pages.CRUD
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PublicProject.Data.ApplicationDbContext _context;
+
+        public IndexModel(PublicProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Blog> Blog { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Blog != null)
+            {
+                Blog = await _context.Blog.ToListAsync();
+            }
+        }
+    }
+}
