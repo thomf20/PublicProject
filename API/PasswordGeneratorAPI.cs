03c0668ec7f1fdb21b566d8236c03058bf47d245
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2023-05-18 15:36:12 +0200

    Fixat Password api
    Fixat ett fungerande password api som funkar nu

---
+++ API/PasswordGeneratorAPI.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace PublicProject.API
+{
+    public  class PasswordGeneratorAPI
+    {
+       
+        public static async Task<List<Models.Password>> GetRandomPassword()
+        {
+            //string RandomGeneratedPassword = "";
+            List<Models.Password> RandomGeneratedPassword = null;
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44304");
+
+                HttpResponseMessage response = await client.GetAsync("/api/Password");
+                if (response.IsSuccessStatusCode)
+                {
+                    string responsestring = await response.Content.ReadAsStringAsync();
+                    //RandomGeneratedPassword = JsonSerializer.Deserialize<string>(responsestring);
+                    RandomGeneratedPassword = JsonSerializer.Deserialize<List<Models.Password>>(responsestring);
+                }
+            }
+            return RandomGeneratedPassword;
+        }
+    }
+}
---
+++ Models/Password.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace PublicProject.Models
+{
+    public class Password
+    {
+        [JsonPropertyName("passwords")]
+        public string Passwords { get; set; }
+    }
+}
