c9b6489fb62ac15ecd641e4f084ed49a2bd6a784
KaveCoding <teachereli@outlook.com>
2023-05-19 14:19:41 +0200

    neurontemplate tillagd

---
+++ Pages/Neuron.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PublicProject.Models;
+using System.Diagnostics.Metrics;
+
+namespace PublicProject.Pages
+{
+    public class Neuron : PageModel
+    {
+        private readonly Data.ApplicationDbContext DBContext; // Byt ut "YourDbContext" med namnet på din databaskontext
+
+        public Blog BlogPost { get; set; }
+
+        public Neuron(Data.ApplicationDbContext dbContext) // Byt ut "YourDbContext" med namnet på din databaskontext
+        {
+            DBContext = dbContext;
+        }
+
+
+        public IActionResult OnGet(int id)
+        {
+
+            BlogPost = DBContext.Blog.Find(id);
+
+
+            if (BlogPost.Popularity == null)
+            {
+                BlogPost.Popularity = 0;
+            }
+
+            BlogPost.Popularity++;
+
+            if (BlogPost == null)
+            {
+                return NotFound();
+            }
+
+            DBContext.SaveChanges();
+            return Page();
+        }
+    }
+}
---
+++ Pages/PartialViewer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PublicProject
+{
+    public class PartialViewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
