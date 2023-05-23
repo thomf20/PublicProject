d352867d614c3a0351616226ad9e63dac7f32a17
KaveCoding <teachereli@outlook.com>
2023-05-23 09:02:37 +0200

    små fixes

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,56 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PublicProject.Models
 {
-    //    public class Category
-    //    {
-    //        public int Id { get; set; }
-    //        public int Name { get; set; }
-    //        public List<SubCategory>? SubCategories { get; set; }
-    //    }
-
-    //    public class SubCategory
-    //    {
-    //        public int Id { get; set; }
-    //        public int Name { get; set; }
-    //        public List<Blog>? Blogs { get; set; }
-    //    }
-
-    //    public class Blog
-    //    {
-    //        public int Id { get; set; }
-    //        public string Title { get; set; }
-    //        public string Text { get; set; }
-    //        public string Image { get; set; }
-    //        public DateTime Date { get; set; }
-    //        public string UserId { get; set; }
-    //        public int? Popularity { get; set; } 
-    //        public string? Facebook { get; set; }
-    //        public string? Instagram { get; set; }
-    //        public string? YouTube { get; set; }
-    //        public string? Twitter { get; set; }
-    //        //public int Style { get; set; }
-    //        public List <Comment>? Comments { get; set; }
-    //    }
-
-    //    public class Comment
-    //    {
-    //        public string Id { get; set; }
-    //        public string Author { get; set; }
-    //        public string Text { get; set; }
-    //        public string? Image { get; set; }
-    //        public DateTime Date { get; set; }
-    //    }
-    //}
 
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
-
- 
-    
-
-
 }
