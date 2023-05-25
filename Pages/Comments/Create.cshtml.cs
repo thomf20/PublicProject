1c57cddbb5b43fd48db558937ddb17e2ecc2a278
KaveCoding <teachereli@outlook.com>
2023-05-25 13:19:21 +0200

    ej testad

---
+++ Pages/Comments/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PublicProject.Data;
+using PublicProject.Models;
+
+namespace PublicProject.Pages.Comments
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PublicProject.Data.ApplicationDbContext _context;
+
+        public CreateModel(PublicProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Comment Comment { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Comments.Add(Comment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Comments/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PublicProject.Data;
+using PublicProject.Models;
+
+namespace PublicProject.Pages.Comments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PublicProject.Data.ApplicationDbContext _context;
+
+        public IndexModel(PublicProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Comment> Comment { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Comment = await _context.Comments.ToListAsync();
+        }
+    }
+}
